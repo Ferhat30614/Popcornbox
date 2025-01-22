64db058f6db32e3b2208761892c959b1facea083
Ferhat30614 <149321307+Ferhat30614@users.noreply.github.com>
2025-01-22 18:56:29 +0400

    Tab yapısıyla uğraşıyorum...

--- app/src/main/java/com/example/poppcornapplicationnew/MovieDetails.kt
+++ app/src/main/java/com/example/poppcornapplicationnew/MovieDetails.kt
@@ -26,6 +26,22 @@ class MovieDetails : Fragment() {
         val bundle:MovieDetailsArgs by navArgs()
         val Movie=bundle.myMovie
 
+        fragmentListesi.add(FragmentFilmDetaylar())
+        fragmentListesi.add(FragmentBenzerler())
+
+
+        val adapter = MyViewPagerAdapter(this)
+        viewpager2.adapter = adapter
+
+        fragmentBaslikListesi.add("Bir")
+        fragmentBaslikListesi.add("İki")
+        fragmentBaslikListesi.add("Üç")
+
+        TabLayoutMediator(tablayout,viewpager2){tab, position ->
+            tab.setText(fragmentBaslikListesi[position])
+        }.attach()
+
+
 
 
 
@@ -35,6 +51,18 @@ class MovieDetails : Fragment() {
 
 
         return binding.root
+    }
+
+
+    inner class MyViewPagerAdapter(fragmentActivity: FragmentActivity):FragmentStateAdapter(fragmentActivity){
+        override fun getItemCount(): Int {
+            return fragmentListesi.size
+        }
+
+        override fun createFragment(position: Int): Fragment {
+            return fragmentListesi[position]
+        }
+
     }
 
 
